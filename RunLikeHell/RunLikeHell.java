d1af629eb06eba6e39a05b4908da6264839e8791
SorcerReyn <56052771+SorcerReyn@users.noreply.github.com>
2019-12-01 18:43:03 -0600

    stuff

--- RunLikeHell/RunLikeHell.java
+++ RunLikeHell/RunLikeHell.java
@@ -6,9 +6,27 @@ import java.util.*;
 
 public class RunLikeHell
 {
+    public static int maxGainRec(int [] blocks, int n)
+    {
+        // Base case: there is nothing in the blocks array, or the last blocks are being checked
+        // If none of those, then increment the count
+        if (blocks.length == 0)
+            return 0;
+        else if (n == blocks.length)
+            return blocks[n];
+        else if (n == blocks.length + 1)
+            return blocks[n + 1]
+        else
+            n++;
+
+        if (blocks[n + 2] > blocks [n + 3])
+            return blocks[n++] + maxGainRec(blocks[n + 1])
+    }
+
     public static int maxGain(int [] blocks)
     {
-        
+        int max = 0;
+
     }
 
     public static double difficultyRating()
